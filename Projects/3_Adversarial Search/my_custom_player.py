e826a005cafb2fdb2e0fe51778a359d5b7734820
Yazhuo Li <yli@liyazhuo.com>
2019-03-10 19:57:55 +0400

    First version implementation of Monte Carlo Tree Search

--- Projects/3_Adversarial Search/my_custom_player.py
+++ Projects/3_Adversarial Search/my_custom_player.py
@@ -1,6 +1,9 @@
 
 from sample_players import DataPlayer
+import random
+from math import sqrt, log
 
+SCALER_FACTOR = 1.0/sqrt(2.0)
 
 class CustomPlayer(DataPlayer):
     """ Implement your own agent to play knight's Isolation
@@ -19,6 +22,7 @@ class CustomPlayer(DataPlayer):
       any pickleable object to the self.context attribute.
     **********************************************************************
     """
+
     def get_action(self, state):
         """ Employ an adversarial search technique to choose an action
         available in the current state calls self.queue.put(ACTION) at least
@@ -42,5 +46,92 @@ class CustomPlayer(DataPlayer):
         # EXAMPLE: choose a random move without any search--this function MUST
         #          call self.queue.put(ACTION) at least once before time expires
         #          (the timer is automatically managed for you)
-        import random
-        self.queue.put(random.choice(state.actions()))
+        #import random
+        if state.ply_count < 2:            
+            self.queue.put(random.choice(state.actions()))
+        else:
+            self.queue.put(random.choice(state.actions()))
+            if not state.terminal_test():
+                mcts = MonteCarloTreeSearch(state, SCALER_FACTOR)
+                mcts.run_search()
+                self.queue.put(mcts.best_actions())
+
+class GameTreeNode():
+    def __init__(self, state, parent = None):
+        self.state = state
+        self.parent = parent
+        self.children = []
+        self.num_visited = 0
+        self.reward = 0
+        self.untried_actions = state.actions()
+    
+    def Expand(self):
+        a = random.choice(self.untried_actions())
+        new_child = GameTreeNode(self.state.result(a), parent=self)
+        new_child.children = a
+        return new_child
+    
+    def best_child(self, scale_factor):
+        f = lambda child : child.reward/child.num_visited + scale_factor*sqrt(2.0*log(self.num_visited)/child.num_visited)
+        bc = max(self.children, key = f)
+        return bc 
+
+    def is_expandable(self):
+        if not self.is_terminal() and self.children :
+            return True
+        else:
+            return False
+
+    def is_terminal(self):
+        return self.state.terminal_test()
+
+ 
+
+
+
+class MonteCarloTreeSearch():
+
+    def __init__(self, state, scale_factor):
+        self.node = GameTreeNode(state)
+        self.scale_factor = scale_factor
+
+
+    def tree_policy(self, node):
+        while not node.is_terminal :
+            if node.is_expandable():
+                return node.Expand()
+            else:
+                node = node.best_child(SCALER_FACTOR)
+                print (node)
+        return node
+
+    def default_policy(self, state):
+        while not state.terminal_test():
+            a = random.choice(state.actions())
+            new_state = state.result(a)
+        return self.state_reward(new_state)
+
+    def state_reward(self, state):
+        if state.utility(state.player()) == float("inf"):
+            return -1.0
+        elif state.utility(state.player()) == float("-inf"):
+            return 1.0
+        else:
+            return 0
+
+    def back_up_negameax(self, node, delta):
+        while node.parent :
+            node.num_visited = node.num_visited + 1
+            node.reward = node.reward + delta
+            delta = -delta
+            node = node.parent 
+
+    def run_search(self):
+        start_node = self.tree_policy(self.node)
+        start_state = start_node.state
+        delta = self.default_policy(start_state)
+        self.back_up_negameax(start_node, delta)
+
+    def best_actions(self):
+        return self.node.best_child
+
