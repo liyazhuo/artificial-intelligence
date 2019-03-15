46fee78e19e8a4b227d3bdac6dbfa1018d7c927b
Yazhuo Li <yli@liyazhuo.com>
2019-03-15 13:47:59 +0400

    Finalised MCTS, code cleaned up

--- Projects/3_Adversarial Search/my_custom_player.py
+++ Projects/3_Adversarial Search/my_custom_player.py
@@ -3,8 +3,6 @@ from sample_players import DataPlayer
 import random
 from math import sqrt, log
 
-SCALER_FACTOR = 1.0/sqrt(2.0)
-
 class CustomPlayer(DataPlayer):
     """ Implement your own agent to play knight's Isolation
 
@@ -46,92 +44,97 @@ class CustomPlayer(DataPlayer):
         # EXAMPLE: choose a random move without any search--this function MUST
         #          call self.queue.put(ACTION) at least once before time expires
         #          (the timer is automatically managed for you)
-        #import random
-        if state.ply_count < 2:            
+
+        #Select a random move at the beginning of a game
+        if state.ply_count < 2:
             self.queue.put(random.choice(state.actions()))
         else:
-            self.queue.put(random.choice(state.actions()))
+
+            self.queue.put(random.choice(state.actions())) #call self.queue.put(ACTION) at least once          
             if not state.terminal_test():
-                mcts = MonteCarloTreeSearch(state, SCALER_FACTOR)
-                mcts.run_search()
-                self.queue.put(mcts.best_actions())
+                '''Monte Carlo Tree Search '''
+                mcts = MonteCarloTreeSearch(GameTreeNode(state), e_e_ratio = 1.414)
+                #e_e_ratio is the ratio between exploitation and exploration
+                while True:
+                    mcts.run_search()
+                    best_action = mcts.best_action()
+                    self.queue.put(best_action)
 
 class GameTreeNode():
     def __init__(self, state, parent = None):
         self.state = state
         self.parent = parent
-        self.children = []
+        self.children = {}
         self.num_visited = 0
         self.reward = 0
         self.untried_actions = state.actions()
     
-    def Expand(self):
-        a = random.choice(self.untried_actions())
-        new_child = GameTreeNode(self.state.result(a), parent=self)
-        new_child.children = a
+    def Expand(self): #Node expansion
+        next_action = self.untried_actions.pop(random.randint(0, len(self.untried_actions)-1))
+        new_child = GameTreeNode(self.state.result(next_action), parent=self)
+        self.children[new_child] = next_action 
+        #Here children dictionary stores all the actions while keys are child nodes
         return new_child
-    
-    def best_child(self, scale_factor):
-        f = lambda child : child.reward/child.num_visited + scale_factor*sqrt(2.0*log(self.num_visited)/child.num_visited)
-        bc = max(self.children, key = f)
-        return bc 
 
-    def is_expandable(self):
-        if not self.is_terminal() and self.children :
-            return True
-        else:
-            return False
+    def best_uct(self, e_e_ratio):
+        #UCT algorithm for node selections
+        best_child = max(self.children.keys(), key = lambda child: child.reward/child.num_visited + e_e_ratio * sqrt(log(self.num_visited)/child.num_visited))
+        return best_child
+
 
     def is_terminal(self):
         return self.state.terminal_test()
 
- 
+    def best_action(self):
+        #action associated with the best node
+        best_child = max(self.children.keys(), key = lambda c: c.num_visited)
+        return self.children[best_child]
 
 
 
 class MonteCarloTreeSearch():
 
-    def __init__(self, state, scale_factor):
-        self.node = GameTreeNode(state)
-        self.scale_factor = scale_factor
+    def __init__(self, node, e_e_ratio):
+        self.root = node
+        self.e_e_ratio = e_e_ratio
 
 
-    def tree_policy(self, node):
-        while not node.is_terminal :
-            if node.is_expandable():
-                return node.Expand()
+    def tree_policy(self): 
+        current_node = self.root
+        while not current_node.is_terminal() :
+            if current_node.untried_actions:
+                return current_node.Expand()
             else:
-                node = node.best_child(SCALER_FACTOR)
-                print (node)
-        return node
+                current_node = current_node.best_uct(self.e_e_ratio)
+        return current_node
+    
 
-    def default_policy(self, state):
+
+    def default_policy(self, state):# the default policy used to simulate 
         while not state.terminal_test():
-            a = random.choice(state.actions())
-            new_state = state.result(a)
-        return self.state_reward(new_state)
+            state = state.result(random.choice(state.actions())) #uniformed node slections
+        return self.state_reward(state)
 
-    def state_reward(self, state):
+    def state_reward(self, state): #reward policy
         if state.utility(state.player()) == float("inf"):
             return -1.0
         elif state.utility(state.player()) == float("-inf"):
             return 1.0
         else:
-            return 0
+            return 0.0
 
-    def back_up_negameax(self, node, delta):
-        while node.parent :
+
+    def back_up_negameax(self, node, delta): # backpropagations after simulation
             node.num_visited = node.num_visited + 1
             node.reward = node.reward + delta
-            delta = -delta
-            node = node.parent 
+            if node.parent:
+                self.back_up_negameax(node.parent, -delta)
 
     def run_search(self):
-        start_node = self.tree_policy(self.node)
-        start_state = start_node.state
-        delta = self.default_policy(start_state)
+        start_node = self.tree_policy()
+        delta = self.default_policy(start_node.state)
         self.back_up_negameax(start_node, delta)
 
-    def best_actions(self):
-        return self.node.best_child
+    def best_action(self):
+        return self.root.best_action()
 
